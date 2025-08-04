d2088085384da9256f9f7ae0260b8d0710ca6cb5
aleandremanueldsaco <amsaco86868@liceo.edu.ph>
2025-08-04 10:25:17 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,56 @@
-import java.util.Scanner;
-public class Main {
-    static String studentId = "";
-    static String firstname = "";
-    static String lastname = "";
-    static String course = "";
-    static String section = "";
-    static int midterm = 0;
-    static int finals = 0;
-    static int project = 0;
-    static int attendance = 0;
-    static int averageScore = 0;
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+import java.sql.SQLOutput;
 
-        System.out.print("Student ID: ");
-        studentId = scanner.nextLine();
-        System.out.print("First Name: ");
-        firstname = scanner.nextLine();
-        System.out.print("Last Name: ");
-        lastname = scanner.nextLine();
-        System.out.print("Course: ");
-        course = scanner.nextLine();
-        System.out.print("Section: ");
-        section = scanner.nextLine();
-        System.out.println("Student ID:" + studentId);
-        System.out.println("First Name:" + firstname);
-        System.out.println("Last Name:" + lastname);
-        System.out.println("Course:" + course);
-        System.out.println("Section:" + section);
+class Book {
+    String title ="";
+    String author = "";
+    int pages = 0;
+    boolean isAvailable = true;
 
-
-        System.out.println("Enter your score");
-
-        System.out.print("Midterm Exam Score: ");
-        midterm = scanner.nextInt();
-
-        System.out.print("Final Exam Score: ");
-        finals = scanner.nextInt();
-
-        System.out.print("Project Score: ");
-        project = scanner.nextInt();
-
-        System.out.print("Attendance Score: ");
-        attendance = scanner.nextInt();
-
-        System.out.println("STUDENT SCORE");
-        System.out.println("Midterm Exam Score: " + midterm);
-        System.out.println("Final Exam Score:" + finals);
-        System.out.println("Project Score:" + project);
-        System.out.println("Attendance Score:" + attendance);
-        averageScore = midterm + finals + project + attendance / 400;
-
-        System.out.println("Average Score: " + averageScore);
-        if(averageScore >= 75) {
-            System.out.println("Remarks:PASSED");
-        } else {
-            System.out.println("Remarks:FAILED");
+    public Book(String titleName, String authorName, int numberPages){
+        title = titleName;
+        author = authorName;
+        pages = numberPages;
+        System.out.println("A new book " + title + " " + " by " + author + " " + " has been added to the library");
+    }
+    public void displayInfo(){
+        if(title == "The Great Gatsby") {
+            System.out.println("The Great Gatsby (/ɡætsbiː/ ⓘ) is a 1925 novel by American writer F. Scott Fitzgerald.");
         }
+        if(title == "To Kill a Mockingbird") {
+            System.out.println("To Kill a Mockingbird is a 1960 Southern Gothic novel by American author Harper Lee.");
+        }
+        if(title == "Pride and Prejudice"){
+            System.out.println("Pride and Prejudice is the second published novel (but third to be written) by English author Jane Austen");
+        }
+    }
+    public void borrowBook(){
+        isAvailable = false;
+        System.out.println("This book " + title + " not availabe" );
 
+    }
+    public void returnBook(){
+        isAvailable = true;
+        System.out.println("This book " + title + " is Available");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println();
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 220);
+        book1.displayInfo();
+        book1.borrowBook();
+        book1.borrowBook();
+
+        System.out.println();
+        Book book2 = new Book("To Kill a Mockingbird", "Harpes Lee",336);
+        book2.displayInfo();
+        book2.borrowBook();
+        book2.borrowBook();
+
+        System.out.println();
+        Book book3 = new Book("Pride and Prejudice", "Jane Austen", 473 );
+        book3.displayInfo();
+        book3.borrowBook();
+        book3.borrowBook();
     }
 }
